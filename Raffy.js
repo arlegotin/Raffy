6d6c9243757756072ee0986cf31d65fbd5031e7a
Artem Legotin <arlegotin@gmail.com>
2015-08-20 00:14:21 +0300

    global fix

--- Raffy.js
+++ Raffy.js
@@ -1,37 +1,39 @@
 ( function( window, undefined ) {
-    var Raffy = function( loop ) {
-        var loopFunction = loop,
-            raf_id,
-            rafStart = window.requestAnimationFrame,
-            rafStop = window.cancelAnimationFrame;
-        
-        var step = function() {
-            loopFunction();
-            raf_id = rafStart( step );
-        };
-        
-        this.start = function() {
-            if ( raf_id === undefined ) {
-                step();
-                
-                return true;
-            } else {
-                return false;
-            }
-        };
-        
-        this.stop = function() {
-            if ( raf_id !== undefined ) {
-                rafStop( raf_id );
-                
-                raf_id = undefined;
-                
-                return true;
-            } else {
-                return false;
-            }
-        };
-    };
+	var Raffy = function( loopFn ) {
+		this.setLoop( loopFn );
+		this._raf_id;
+		this._tStep = this._step.bind( this );
+	};
+	
+	Raffy.prototype.setLoop = function( loopFn ) {
+		this._loopFn = loopFn || function() {};
+	};
+	
+	Raffy.prototype._rafStart = window.requestAnimationFrame.bind( window );
+	Raffy.prototype._rafStop = window.cancelAnimationFrame.bind( window );
+	
+	Raffy.prototype._step = function() {
+		this._loopFn();
+		this._raf_id = this._rafStart( this._tStep );
+	};
+	
+	Raffy.prototype.start = function() {
+		if ( this._raf_id === undefined ) {
+			this._step();
+			return true;
+		} else {
+			return false;
+		}
+	};
+	
+	Raffy.prototype.stop = function() {
+		if ( this._raf_id !== undefined ) {
+			this._rafStop( this._raf_id );
+			return true;
+		} else {
+			return false;
+		}
+	};
     
     window.Raffy = window.Raffy || Raffy;
 } )( window );
