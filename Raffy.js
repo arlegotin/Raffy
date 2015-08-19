125d55a72b4438c23f7b302c78b3e63e1e11a6ea
Artem Legotin <arlegotin@gmail.com>
2015-08-20 00:15:34 +0300

    markup fix

--- Raffy.js
+++ Raffy.js
@@ -1,39 +1,39 @@
 ( function( window, undefined ) {
-	var Raffy = function( loopFn ) {
-		this.setLoop( loopFn );
-		this._raf_id;
-		this._tStep = this._step.bind( this );
-	};
-	
-	Raffy.prototype.setLoop = function( loopFn ) {
-		this._loopFn = loopFn || function() {};
-	};
-	
-	Raffy.prototype._rafStart = window.requestAnimationFrame.bind( window );
-	Raffy.prototype._rafStop = window.cancelAnimationFrame.bind( window );
-	
-	Raffy.prototype._step = function() {
-		this._loopFn();
-		this._raf_id = this._rafStart( this._tStep );
-	};
-	
-	Raffy.prototype.start = function() {
-		if ( this._raf_id === undefined ) {
-			this._step();
-			return true;
-		} else {
-			return false;
-		}
-	};
-	
-	Raffy.prototype.stop = function() {
-		if ( this._raf_id !== undefined ) {
-			this._rafStop( this._raf_id );
-			return true;
-		} else {
-			return false;
-		}
-	};
+    var Raffy = function( loopFn ) {
+        this.setLoop( loopFn );
+        this._raf_id;
+        this._tStep = this._step.bind( this );
+    };
+    
+    Raffy.prototype.setLoop = function( loopFn ) {
+        this._loopFn = loopFn || function() {};
+    };
+    
+    Raffy.prototype._rafStart = window.requestAnimationFrame.bind( window );
+    Raffy.prototype._rafStop = window.cancelAnimationFrame.bind( window );
+    
+    Raffy.prototype._step = function() {
+        this._loopFn();
+        this._raf_id = this._rafStart( this._tStep );
+    };
+    
+    Raffy.prototype.start = function() {
+        if ( this._raf_id === undefined ) {
+            this._step();
+            return true;
+        } else {
+            return false;
+        }
+    };
+    
+    Raffy.prototype.stop = function() {
+        if ( this._raf_id !== undefined ) {
+            this._rafStop( this._raf_id );
+            return true;
+        } else {
+            return false;
+        }
+    };
     
     window.Raffy = window.Raffy || Raffy;
 } )( window );
